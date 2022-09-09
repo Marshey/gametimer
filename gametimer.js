679dc00c3e78339a617fe65bcb9292aaa0fdb922
Marshey <platecdust@gmail.com>
2022-09-09 09:58:37 +0900

    ボタン動作の変更

--- gametimer.js
+++ gametimer.js
@@ -2,11 +2,13 @@ let isNotify;       //通知の可否
 let intervIdArr = new Array();
 
 let stamina = {};   //スタミナ
+let initialTime;    //タイマー開始時刻
 let targetTime;     //完了時刻
 let lastElapsed;    //最後に計算が行われた時刻
 let flow;           //floorの溢れ
 
 window.addEventListener('DOMContentLoaded', () => {
+    updateElement();
     document.getElementById('notifyChk').addEventListener('change', function () {
         if (this.checked) {
             isNotify = true;
@@ -15,42 +17,47 @@ window.addEventListener('DOMContentLoaded', () => {
             isNotify = false;
         }
     });
+    document.getElementById('start_bt').onclick = function startTimer() {
+        if (intervIdArr.length > 0) {
+            clearInterval(intervIdArr.shift());
+        }
+
+        getStamina();
+        document.getElementById('curSt').max = stamina.maxStamina;
+        displayProgress(stamina.currentStamina, stamina.maxStamina);
+        if (stamina.currentStamina <= stamina.maxStamina) {
+            initialTime = new Date().getTime();
+            targetTime = initialTime + ((stamina.maxStamina - stamina.currentStamina) * stamina.intervTime * 1000);
+            lastElapsed = initialTime;
+            flow = 0;
+            updateElement();
+            intervIdArr.push(setInterval(timer, 1000));
+        }
+    }
 });
 
-function startTimer() {
-    if (intervIdArr.length > 0) {
-        clearInterval(intervIdArr.shift());
-    }
-    stamina.intervTime = parseFloat(document.getElementById('intTime').value);
-    stamina.currentStamina = parseInt(document.getElementById('curSt').value);
-    stamina.maxStamina = parseInt(document.getElementById('maxSt').value);
-    document.getElementById('curSt').max = stamina.maxStamina;
-
-    displayProgress(stamina.currentStamina, stamina.maxStamina);
-
-    if (stamina.currentStamina <= stamina.maxStamina) {
-        const initialTime = new Date().getTime();
-        targetTime = initialTime + ((stamina.maxStamina - stamina.currentStamina) * stamina.intervTime * 1000);
-        lastElapsed = initialTime;
-        flow = 0;
-        intervIdArr.push(setInterval(timer, 1000));
-    }
-}
-
 function updateTimer() {
+    getStamina();
     let addStamina = parseInt(document.getElementById('addSt1').value);
+    targetTime = initialTime + ((stamina.maxStamina - stamina.currentStamina) * stamina.intervTime * 1000);
 
-    if (stamina.currentStamina + addStamina >= 0 && stamina.currentStamina + addStamina <= stamina.maxStamina) {
+    if (stamina.currentStamina + addStamina >= 0) {
         stamina.currentStamina += addStamina;
         targetTime -= addStamina * stamina.intervTime * 1000;
-        displayProgress(stamina.currentStamina, stamina.maxStamina);
     }
+    if (stamina.currentStamina + addStamina >= stamina.maxStamina) {
+        stamina.currentStamina = stamina.maxStamina;
+        targetTime = new Date().getTime();
+    }
+    displayProgress(stamina.currentStamina, stamina.maxStamina);
 }
 
+//1秒毎に実行されるメイン処理
 function timer() {
     const now = new Date().getTime();
     const remainTime = targetTime - now;    //残り時間(ms)
 
+    //現在時刻に基づいてスタミナを更新
     if (now - lastElapsed + flow >= stamina.intervTime * 1000) {
         stamina.currentStamina += Math.floor((now - lastElapsed + flow) / 1000 / stamina.intervTime);
         flow = (now - lastElapsed + flow) % (stamina.intervTime * 1000);
@@ -64,10 +71,15 @@ function timer() {
         if (isNotify) {
             notify(stamina.currentStamina);
         }
-        if (intervIdArr.length > 0) {
-            clearInterval(intervIdArr.shift());
-        }
+        updateElement();
+        clearInterval(intervIdArr.shift());
     }
+}
+
+function getStamina() {
+    stamina.intervTime = parseFloat(document.getElementById('intTime').value);
+    stamina.currentStamina = parseInt(document.getElementById('curSt').value);
+    stamina.maxStamina = parseInt(document.getElementById('maxSt').value);
 }
 
 function displayProgress(nowProg, maxProg) {
@@ -82,7 +94,6 @@ function displayProgress(nowProg, maxProg) {
 }
 
 function displayTime(msTime) {
-    // console.log(msTime)
     if (msTime < 0) {
         msTime = 0;
     }
@@ -103,5 +114,40 @@ function notify(stamina) {
                 }
             });
         }
+    }
+}
+
+function updateElement() {
+    const containerElement = document.getElementById('container');
+    if (!(document.getElementById('start_bt'))) {
+        if (document.getElementById('lavelAddSt1')) {
+            document.getElementById('lavelAddSt1').remove();
+            document.getElementById('update_bt').remove();
+        }
+
+        const startBtElement = document.createElement('input');
+        startBtElement.setAttribute('id', 'start_bt');
+        startBtElement.setAttribute('type', 'button');
+        startBtElement.setAttribute('value', '開始');
+        containerElement.appendChild(startBtElement);
+
+    } else {
+        document.getElementById('start_bt').remove();
+        const lavelElement = document.createElement('lavel');
+        lavelElement.setAttribute('id', 'lavelAddSt1');
+        lavelElement.textContent = 'スタミナ加算';
+        const updateInElement = document.createElement('input');
+        updateInElement.setAttribute('id', 'addSt1',);
+        updateInElement.setAttribute('type', 'number');
+        updateInElement.setAttribute('value', '0');
+        updateInElement.setAttribute('min', '0');
+        lavelElement.appendChild(updateInElement);
+        containerElement.appendChild(lavelElement);
+        const updateBtElement = document.createElement('input');
+        updateBtElement.setAttribute('id', 'update_bt');
+        updateBtElement.setAttribute('type', 'button');
+        updateBtElement.setAttribute('value', '更新');
+        updateBtElement.setAttribute('onclick', 'updateTimer()');
+        containerElement.appendChild(updateBtElement);
     }
 }
